f53aa648e66445d7b0f4cb878a627ee086d36219
Gabriel Bonavoluntá <gabriel.bonavolonta@hotmail.com>
2025-10-31 15:04:52 -0300

    Começando a implementar o método que imprime o tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using tabuleiro;
 
 namespace xadrez_console
@@ -12,6 +8,8 @@ namespace xadrez_console
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Tela.imprimirTabuleiro(tab);
         }
     }
 }
---
+++ xadrez-console/Tela.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using tabuleiro;
+
+
+namespace xadrez_console
+{
+    internal class Tela
+    {
+        public static void imprimirTabuleiro(Tabuleiro tab)
+        {
+            for (int i = 0; i < tab.linhas; i++)
+            {
+                for (int j = 0; j < tab.colunas; j++)
+                {
+                    if (tab.peca == null){
+                        Console.Write(tab.peca(i, j) + " ");
+                    } else
+                    {
+                        Console.Write(" - ");
+                    }
+                    
+                }
+                Console.WriteLine();
+            }
+}
+    }
+}
